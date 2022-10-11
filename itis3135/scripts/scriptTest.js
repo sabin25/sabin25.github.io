01c56904e8e8826b49a76c7063ff4f4f3331ed1d
Sabin Shrestha <sabinsabie@gmail.com>
2022-10-11 15:51:37 -0400

    x

--- itis3135/scripts/scriptTest.js
+++ itis3135/scripts/scriptTest.js
@@ -1,9 +1,10 @@
 let firstInput = document.querySelector('#firstInput');
 let secondInput = document.querySelector('#secondInput');
 let btnAdd = document.querySelector('button');
-let result = document.querySelector('h2');
+let result = document.querySelector('h4');
 
 btnAdd.addEventListener('click', () =>{
     let total = firstInput.value + secondInput.value;
     result.innerText = total;
 })
+
