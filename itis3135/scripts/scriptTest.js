8e860ff993616d80ddc794122fc50fe865155692
Sabin Shrestha <sabinsabie@gmail.com>
2022-10-11 14:53:57 -0400

    ds

---
+++ itis3135/scripts/scriptTest.js
@@ -0,0 +1,14 @@
+let btnShow = document.querySelector('button');
+let output = document.querySelector('h1');
+
+btnShow.addEventListener('click', () => {
+    let today = new Date();
+
+    let month =today.getMonth() + 1;
+    let year = today.getFullYear();
+    let data = today.getDate();
+
+    let current_date = `${month}/${date}/${year}`;
+    output.innerText = current_date;
+
+});
