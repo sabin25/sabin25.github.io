1ddf25e787608d6cd6c3b04649762d6d60b49150
Sabin Shrestha <sabinsabie@gmail.com>
2022-10-15 20:31:39 -0400

    v

---
+++ itis3135/scripts/polygon.js
@@ -0,0 +1,64 @@
+function getShape(){  
+    var text;
+    var sides = prompt("The angry lion would like your number:"); 
+    
+    
+    switch(sides) {
+        case "1":
+            alert("Invalid! Enter a number between 3 to 15");
+            break;
+            case "2":
+            alert("Invalid! Enter a number between 3 to 15");
+            break;
+            case "3":
+            alert("Number of sides = 3\nName of polygon = Triangle");
+            break;
+             case "4":
+                alert("Number of sides = 4\nName of polygon = Tetragon");
+                break;
+                case "5":
+                    alert("Number of sides = 5\nName of polygon = Pentagon");
+                    break;
+                    case "6":
+                        alert("Number of sides = 6\nName of polygon = Hexagon");
+                        break;
+                        case "7":
+                            alert("Number of sides = 7\nName of polygon = Heptagon");
+                            break;
+                            case "8":
+                            alert("Number of sides = 8\nName of polygon = Octagon");
+                            break;
+                            case "9":
+                            alert("Number of sides = 9\nName of polygon = Nonagon");
+                            break;
+                            case "10":
+                            alert("Number of sides = 10\nName of polygon = Decagon");
+                            break;
+                            case "11":
+                            alert("Number of sides = 11\nName of polygon = Hendecagon");
+                            break;
+                            case "12":
+                            alert("Number of sides = 12\nName of polygon = Dodecagon");
+                            break;
+                            case "13":
+                                alert("Number of sides = 13\nName of polygon = Triskaidecagon");
+                                break;
+                                case "14":
+                                    alert("Number of sides = 14\nName of polygon = Triskaidecagon");
+                                    break;
+                                    case "15":
+                                        alert("Number of sides = 15\nName of polygon = Triskaidecagon");
+                                        break;
+                                                    
+                            default:
+                                
+                            getShape();
+                            
+                            
+                break;
+  }}
+  getShape();
+
+  function validateEntry(){
+
+  }
