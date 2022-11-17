44915d66741864802af0dd1d31800d91a1a51f1b
Sabin Shrestha <sabinsabie@gmail.com>
2022-11-16 20:23:13 -0500

    ImageSwap

--- itis3135/scripts/Activity10.js
+++ itis3135/scripts/Activity10.js
@@ -1,3 +1,11 @@
+
+const thumbs=document.querySelector(".thumb-img").children;
+
 function changeImage(event){
-    console.log(event)
-}
+   document.querySelector(".pro-img").src=event.children[0].src
+   
+   for(let i=0; i<thumbs.length;i++){
+    thumbs[i].classList.remove("active");
+   }
+    event.classList.add("active");
+   }
