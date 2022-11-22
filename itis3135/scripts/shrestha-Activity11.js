9f4995b201f092071e1d9a7fb42701e44c239558
Sabin Shrestha <sabinsabie@gmail.com>
2022-11-21 23:32:13 -0500

    updated activity11.html

--- itis3135/scripts/shrestha-Activity11.js
+++ itis3135/scripts/shrestha-Activity11.js
@@ -1,4 +1,11 @@
-<!-- jQuery call to the accordion() method. -->
 $(document).ready(function() {
-    $("#tabs").tabs();
+    $("#tabs").accordion({
+
+    collapsible: true,
+
+    active: false
 });
+
+});
+
+
