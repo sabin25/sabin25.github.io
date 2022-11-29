0e820f75f79d9a7cc6f78c5e641e33abb7e9a351
Sabin Shrestha <sabinsabie@gmail.com>
2022-11-28 19:37:15 -0500

    xc

--- itis3135/Activity14/shrestha-Activity14.js
+++ itis3135/Activity14/shrestha-Activity14.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-	
-}); // end ready
+    $("#nav_list li").click(function() {
+    var title = $(this).children("a").attr("title");
+    $.get(title + ".json", function(data, status) {
+    data = data['speakers'][0];
+    $("main h1").html(data['title']);
+    $("main h2").html(data['month']+"<br />"+data['speaker']);
+    $("main img").attr("src", data.image);
+    $("main p").html(data.text);
+    });
+    });
+    });
