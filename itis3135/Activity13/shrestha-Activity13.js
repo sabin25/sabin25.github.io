9378b156539ec02fb2a60be9ba38e6beafe8ca7a
Sabin Shrestha <sabinsabie@gmail.com>
2022-11-28 19:05:42 -0500

    activity13

--- itis3135/Activity13/shrestha-Activity13.js
+++ itis3135/Activity13/shrestha-Activity13.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
-    $.ajax({
-        type: "get",
-        url: "team.xml",
-        beforeSend: function() {
-            $("#team").html("Loading...");
-        },
-        timeout: 10000,
-        error: function(xhr, status, error) {
-            alert("Error: " + xhr.status + " - " + error);
-        },
-        dataType: "xml",
-        success: function(data) {
-            $("#team").html("");
-            $(data).find("management").children().each(function() {
-                var xmlDoc = $(this);
-                $("#team").append
-                ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
-                          xmlDoc.find("title").text() + "<br>" +
-                          xmlDoc.find("bio").text() + "<br>");
+$.ajax({
+    type: "get",
+    url: "team.json",
+    beforeSend: function() {
+        $("#team").html("Loading...");
+    },
+    timeout: 10000,
+    error: function(xhr, status, error) {
+        alert("Error: " + xhr.status + " - " + error);
+    },
+    dataType: "json",
+    success: function(data) {
+        $("#team").html("");
+        $.each(data, function() {
+        $.each(this, function(key, value) {
+            $("#team").append(
+                "Name: " + value.name + "<br>" +
+                "Title: " + value.title + "<br>" +
+                "Bio: " + value.bio + "<br><br>"
+                );
             });
-        }
-    });
+        });
+    }
 });
+    
