300a06b4c1d3868ff0226e1ce77ed0c9c7ff4d96
Sabin Shrestha <sabinsabie@gmail.com>
2022-12-10 14:17:31 -0500

    m

--- itis3135/clientProject/spices.js
+++ itis3135/clientProject/spices.js
@@ -1,3 +1,38 @@
-$( function() {
-    $( "#accordion" ).accordion();
-  } );
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+
+  });
+}
+
+
+
+function startTime() {
+  
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('clock').style.textAlign = "center";
+
+    setTimeout(startTime, 1000);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  
+    return i;
+  }
